62f70594d277de5fd83b38285337c12ef61839f8
zuberypim <zuberypima12@gmail.com>
2022-08-23 16:19:07 +0300

    this is my first django project on github

---
+++ drinks/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Drinks
+
+admin.site.register(Drinks)
+
---
+++ drinks/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import field
+from  rest_framework import serializers
+from .models import Drinks
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Drinks
+        fields =['id','name','description']
